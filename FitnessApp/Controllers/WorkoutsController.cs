44ac347e6a2e2f975d197255cb405925d350fc78
Caleb Rider <caleb.j.rider@gmail.com>
2023-10-28 18:53:34 -0400

    created and tested REST API requests

--- FitnessApp/Controllers/WorkoutsController.cs
+++ FitnessApp/Controllers/WorkoutsController.cs
@@ -4,26 +4,27 @@ using FitnessApp.Contracts.Workout;
 namespace FitnessApp.Controllers;
 
 [ApiController]
+[Route("[controller]")]
 public class WorkoutsController : ControllerBase {
-    [HttpPost("/workouts")]
+    [HttpPost()]
     public IActionResult CreateWorkout(CreateWorkoutRequest request)
     {
         return Ok(request);
     }
 
-    [HttpGet("/workouts/{id:guid}")]
+    [HttpGet("{id:guid}")]
     public IActionResult GetWorkout(Guid id)
     {
         return Ok(id);
     }
 
-    [HttpPut("/workouts/{id:guid}")]
+    [HttpPut("{id:guid}")]
     public IActionResult UpsertWorkout(Guid id, UpsertWorkoutRequest request)
     {
         return Ok(request);
     }
 
-    [HttpDelete("/workouts/{id:guid}")]
+    [HttpDelete("{id:guid}")]
     public IActionResult DeleteWorkout(Guid id)
     {
         return Ok(id);
